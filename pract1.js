ee848cdc012ddf8d24b62ba449f04fc526b0f795
Josz27 <36236045+Josz27@users.noreply.github.com>
2019-02-20 23:47:54 -0600

    Práct1
    Nada

---
+++ pract1.js
@@ -0,0 +1,21 @@
+﻿
+//function statement
+let greet = ()=>{
+    console.log("Qué tranza");
+}
+greet();
+
+//function are first-class
+let  logGreeting = (funcion)=>{
+    funcion();
+}
+logGreeting(greet);
+
+//function expression
+let greetMe = ()=>{
+    console.log("Konichiwa =) =)");
+}
+greetMe();
+
+logGreeting(greetMe);
+logGreeting(()=>{console.log("Los AD2000X Son sorprendentes , Dayo-nee?");})
